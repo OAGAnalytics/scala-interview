7aa6c8b5dd3c114a42d55419fae43d9b36e2a389
Daniel Bingham <git@dbingham.com>
2017-12-21 15:40:59 -0800

    Interview Task

---
+++ src/main/scala/com/oag/interview/CsvTransformer.scala
@@ -0,0 +1,182 @@
+package com.oag.interview
+
+import com.opencsv.CSVParserBuilder
+import java.io.PrintWriter
+import java.nio.file.{Files, Path}
+import java.time.LocalDate
+import resource.managed
+import scala.io.Source
+import scala.util.Try
+
+/**
+  * The goal of this class is to transform a daily production log into a production-by-operated-data summary.
+  *
+  * That is, the daily production log has one row of production data per calendar day per well.  A by-operated-day
+  * summary contains one row per well with columns for various time interval production totals (30, 60, 90, etc days).
+  *
+  * For example, the column headers for an example by-operated-day CSV might be:
+  *
+  * wellId, cum_oil_180, cum_oil_360, cum_oil_720
+  *
+  * The starting version of this code produces this structure.  Specifically, you can create an output file for oil,
+  * gas, or water depending on which method you call.
+  *
+  * The goal of the exercise is to add a method that can output by-operated-day summaries for multiple production types
+  * in a single file.  So the headers might look like this for oil, gas, and water all included (order is irrelevant):
+  *
+  * wellId, cum_oil_180, cum_oil_360, cum_oil_720, cum_gas_180, cum_gas_360, cum_gas_720, cum_water_180, cum_water_360, cum_water_720
+  */
+class CsvTransformer(inputFile: Path) {
+  private val wellIdColumn = "wellId"
+  private val dateColumn   = "date"
+  private val oilColumn    = "oil"
+  private val gasColumn    = "gas"
+  private val waterColumn  = "water"
+
+  private val parser = new CSVParserBuilder().build()
+
+  private def wellIdIndex(headers: Array[String]): Int = headers.indexOf(wellIdColumn)
+
+  private def dateIndex(headers: Array[String]): Int = headers.indexOf(dateColumn)
+
+  private def oilIndex(headers: Array[String]): Int = headers.indexOf(oilColumn)
+
+  private def gasIndex(headers: Array[String]): Int = headers.indexOf(gasColumn)
+
+  private def waterIndex(headers: Array[String]): Int = headers.indexOf(waterColumn)
+
+  private def parseDate(dateStr: String): LocalDate = LocalDate.parse(dateStr)
+
+  private def safeToDouble(dblString: String): Double = Try(dblString.toDouble).getOrElse(0.0)
+
+  private def inputFileHeaders: Array[String] = {
+    managed(Files.newInputStream(inputFile)) apply { is =>
+      val production = Source.fromInputStream(is).getLines().toSeq
+      val headers = parser.parseLine(production.head)
+
+      // confirm date parsing on 10 samples
+      val samples = production.tail.take(10).map(parser.parseLine)
+      val dateIndex = this.dateIndex(headers)
+      samples.foreach(a => parseDate(a(dateIndex)))
+
+      headers
+    }
+  }
+
+  def dailyOilToOperatedDay(outputFile: Path, productionIntervals: Seq[Int] = Seq(180, 360, 720)): Unit = {
+    val headers = inputFileHeaders
+
+    val wellIdIndex = this.wellIdIndex(headers)
+    val oilIndex = this.oilIndex(headers)
+
+    dailyToOperatedDay(outputFile,
+                       "oil",
+                       wellIdIndex,
+                       oilIndex,
+                       productionIntervals)
+  }
+
+  def dailyGasToOperatedDay(outputFile: Path, productionIntervals: Seq[Int] = Seq(180, 360, 720)): Unit = {
+    val headers = inputFileHeaders
+
+    val wellIdIndex = this.wellIdIndex(headers)
+    val gasIndex = this.gasIndex(headers)
+
+    dailyToOperatedDay(outputFile,
+                       "gas",
+                       wellIdIndex,
+                       gasIndex,
+                       productionIntervals)
+  }
+
+  def dailyWaterToOperatedDay(outputFile: Path, productionIntervals: Seq[Int] = Seq(180, 360, 720))
+  {
+    val headers = inputFileHeaders
+
+    val wellIdIndex = this.wellIdIndex(headers)
+    val waterIndex = this.waterIndex(headers)
+
+    dailyToOperatedDay(outputFile,
+                       "water",
+                       wellIdIndex,
+                       waterIndex,
+                       productionIntervals)
+  }
+
+  private def dailyToOperatedDay(outputFile: Path,
+                                 productionType: String,
+                                 inputFileWellIdIndex: Int,
+                                 inputFileProductionIndex: Int,
+                                 productionIntervals: Seq[Int] = Seq(180, 360, 720))
+  {
+
+    if(inputFileWellIdIndex < 0 || inputFileProductionIndex < 0)
+      throw new IllegalArgumentException(s"Invalid index, must be greater than 0: inputFileWellIdIndex = $inputFileWellIdIndex; inputFileProductionIndex = $inputFileProductionIndex")
+
+    val wellIds = uniqueWellIds(inputFileWellIdIndex)
+
+    managed(new PrintWriter(Files.newBufferedWriter(outputFile))) apply { writer =>
+      // write headers
+      val intervalHeaders = productionIntervals.map { interval => s"cum_${productionType}_$interval" }
+      writer.println((wellIdColumn +: intervalHeaders).mkString(","))
+
+      wellIds.foreach {
+        wellId =>
+          assert(wellId.nonEmpty, s"Blank WellId")
+
+          val slices = this.slices(wellId, productionIntervals, inputFileProductionIndex)
+
+          val cumProdPerInterval = this.cumProdByInterval(slices, inputFileProductionIndex)
+
+          writer.println((wellId +: cumProdPerInterval).mkString(","))
+      }
+    }
+  }
+
+  // build list of unique WellIds
+  private def uniqueWellIds(wellIdIndex: Int): Set[String] = {
+    managed(Files.newInputStream(inputFile)) apply { is =>
+      Source.fromInputStream(is).getLines().map { l =>
+        val line = parser.parseLine(l)
+        line(wellIdIndex)
+      }.toSet.tail.filter(_.nonEmpty)
+    }
+  }
+
+  private def slices(wellId: String,
+                     productionIntervals: Seq[Int],
+                     prodHeaderIndex: Int): Seq[Seq[Array[String]]] =
+  {
+    // productionInterval days of non-zero production per per window -
+    val data = prodPerWellId(wellId).filter(row => safeToDouble(row(prodHeaderIndex)) > 0.0)
+
+    var sliceStart = 0
+    val intervalSize = productionIntervals.head
+    productionIntervals.map { interval =>
+      val chunk = data.slice(sliceStart, sliceStart + intervalSize)
+      sliceStart = interval
+      chunk
+    }
+  }
+
+  // production per wellId
+  private def prodPerWellId(wellId: String): Seq[Array[String]] = {
+    managed(Files.newInputStream(inputFile)) apply { is =>
+      Source.fromInputStream(is).getLines().filter(_.contains(wellId)).map(parser.parseLine).toList
+    }
+  }
+
+  private def cumProdByInterval(slices: Seq[Seq[Array[String]]], prodIndex: Int): Seq[String] = {
+    var lastInterval = 0.0
+    slices.map { rows =>
+      lastInterval += cum(prodIndex)(rows)
+      s"${(math rint (lastInterval * 100)) / 100}"
+    }
+  }
+
+  private def cum(prodHeaderIndex: Int)(slice: Seq[Array[String]]): Double = {
+    slice.foldLeft(0.0) { case (cumProd, daily) =>
+      cumProd + safeToDouble(daily(prodHeaderIndex))
+    }
+  }
+}
---
+++ src/test/scala/com/oag/interview/CsvTest.scala
@@ -0,0 +1,87 @@
+package com.oag.interview
+
+import com.opencsv.CSVParserBuilder
+import java.nio.file.{Files, Paths}
+import org.scalatest.{FlatSpec, Matchers}
+import resource.managed
+import scala.io.Source
+
+class CsvTest extends FlatSpec with Matchers {
+  private val dataDir = Paths.get("data")
+  private val parser = new CSVParserBuilder().build()
+
+  "csv" should "be created" in {
+    Files.createDirectories(dataDir)
+
+    val dataFile = dataDir.resolve("daily_prod.csv")
+    CsvCreator.createSampleCsv(dataFile)
+
+    Files.exists(dataFile) should be(true)
+  }
+
+  "oil-by-operated-day" should "contain appropriate columns" in {
+    val transformer = new CsvTransformer(dataDir.resolve("daily_prod.csv"))
+    val outFile = dataDir.resolve("oil-by-operated-day.csv")
+    transformer.dailyOilToOperatedDay(outFile)
+
+    val headers = managed(Files.newInputStream(outFile)) apply { is =>
+      parser.parseLine(Source.fromInputStream(is).getLines().toSeq.head)
+    }
+
+    headers.size should be(4)
+    headers(0) should be("wellId")
+    headers(1) should be("cum_oil_180")
+    headers(2) should be("cum_oil_360")
+    headers(3) should be("cum_oil_720")
+  }
+
+  "gas-by-operated-day" should "contain appropriate columns" in {
+    val transformer = new CsvTransformer(dataDir.resolve("daily_prod.csv"))
+    val outFile = dataDir.resolve("gas-by-operated-day.csv")
+    transformer.dailyGasToOperatedDay(outFile)
+
+    val headers = managed(Files.newInputStream(outFile)) apply { is =>
+      parser.parseLine(Source.fromInputStream(is).getLines().toSeq.head)
+    }
+
+    headers.size should be(4)
+    headers(0) should be("wellId")
+    headers(1) should be("cum_gas_180")
+    headers(2) should be("cum_gas_360")
+    headers(3) should be("cum_gas_720")
+  }
+
+  "water-by-operated-day" should "contain appropriate columns" in {
+    val transformer = new CsvTransformer(dataDir.resolve("daily_prod.csv"))
+    val outFile = dataDir.resolve("water-by-operated-day.csv")
+    transformer.dailyWaterToOperatedDay(outFile)
+
+    val headers = managed(Files.newInputStream(outFile)) apply { is =>
+      parser.parseLine(Source.fromInputStream(is).getLines().toSeq.head)
+    }
+
+    headers.size should be(4)
+    headers(0) should be("wellId")
+    headers(1) should be("cum_water_180")
+    headers(2) should be("cum_water_360")
+    headers(3) should be("cum_water_720")
+  }
+
+  // Add a test that calls your new method for Oil and Gas and verifies appropriate output
+
+  // Add a test that calls your new method for Gas and Water and verifies appropriate output
+
+  // Add a test that calls your new method for Oil, Gas, and Water and verifies appropriate output
+
+  "cleanup" should "happen" in {
+    managed(Files.list(dataDir)) apply { stream =>
+      stream.forEach { path =>
+        Files.delete(path)
+      }
+    }
+
+    Files.delete(dataDir)
+
+    Files.exists(dataDir) should be(false)
+  }
+}
